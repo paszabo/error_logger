1ef87f350b3b4d31286c761cf84ca1bcc29fee31
Paul Szabo <paul.szabo@scotiabank.com>
2017-08-24 17:19:16 -0400

    proper file handling

--- error_logger/securities/csv_data.py
+++ error_logger/securities/csv_data.py
@@ -3,7 +3,7 @@ import csv
 from .models import CsvFile
 
 
-def get_headers_and_data(file_name):
+def get_headers_and_data(file_name, strip_blank_lines=True):
     """
     Get headers and data from a saved CSV file
 
@@ -11,21 +11,29 @@ def get_headers_and_data(file_name):
 
     Args:
         file_name: name of the file whose data needs to be retrieved
+        strip_blank_lines: set True if lines of all empty (Python falsey)
+            values should be removed.
+
+            Note: 0 is a falsey value so be careful, a line of all 0s will
+                be stripped if this is true
 
     Returns: 2-tuple of the form (headers, data)
         `headers` is a list of strings
         `data` is a list of lists of strings
     """
-    table_data = CsvFile.objects.get(name=file_name)
+    csv_file = CsvFile.objects.get(name=file_name)
 
-    with open(table_data.file.name) as fp:
-        reader = csv.reader(fp)
-        data = [r for r in reader]
+    csv_file.file.open(mode='rt')
+    reader = csv.reader(csv_file.file)
+    data = [r for r in reader]
+    csv_file.file.close()
+
+    assert csv_file.file.closed
 
     headers = data[0]
     data = data[1:]
 
-    # strip out blank rows
-    data = [d for d in data if any(d)]
+    if strip_blank_lines:
+        data = [d for d in data if any(d)]
 
     return headers, data
